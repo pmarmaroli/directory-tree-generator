11497f8cd54dd7b0fcbdb88c09fc3189a593a5a5
pmarmaroli <38899778+pmarmaroli@users.noreply.github.com>
2024-07-02 18:39:04 +0300

    first commit

---
+++ dtg.ps1
@@ -0,0 +1,45 @@
+﻿param (
+    [string]$rootDirectory
+)
+
+function Get-DirectoryTree {
+    param (
+        [string]$Path,
+        [string]$Indent = "",
+        [bool]$LastChild = $false
+    )
+
+    $items = Get-ChildItem -Path $Path
+    $itemCount = $items.Count
+
+    for ($i = 0; $i -lt $itemCount; $i++) {
+        $item = $items[$i]
+        $isLast = ($i -eq $itemCount - 1)
+        $prefix = "$Indent"
+
+        if ($LastChild) {
+            $prefix += "    "
+        } else {
+            $prefix += "│   "
+        }
+
+        if ($isLast) {
+            $prefix += "└── "
+        } else {
+            $prefix += "├── "
+        }
+
+        if ($item.PSIsContainer) {
+            Write-Output "$prefix$item\"
+            Get-DirectoryTree -Path $item.FullName -Indent "$prefix" -LastChild:$isLast
+        } else {
+            Write-Output "$prefix$item"
+        }
+    }
+}
+
+Write-Output ""
+Write-Output "$rootDirectory\"
+Write-Output ""
+Get-DirectoryTree -Path $rootDirectory -Indent "" -LastChild:$true
+Write-Output ""
